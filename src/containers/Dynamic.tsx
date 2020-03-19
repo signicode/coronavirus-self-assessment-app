71f9a266198fe00fedc4c3e02f6f6c3dd8ef469e
Michal Czapracki <cz@signicode.com>
2020-03-19 17:30:36 +0000

    Cleanup:

--- src/containers/Dynamic.tsx
+++ src/containers/Dynamic.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-type DF = React.FC<{ path?: String }>
+type DF = React.FC<{ path?: string }>
 const Dynamic: DF = () => (
   <div>
     This is a dynamic page! It will not be statically exported, but is available
